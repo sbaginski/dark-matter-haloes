97ee71260bbbed20a3d0e914b6adcee4e3f919d1
sbaginski <s.baginski@student.uw.edu.pl>
2023-11-05 00:09:57 +0100

    fix: Fix incorrect imports

--- src/haloes/process_trees.py
+++ src/haloes/process_trees.py
@@ -3,13 +3,14 @@ from multiprocessing import cpu_count, Pool
 import os
 from pathlib import Path
 
-from toolset.halo_reader import HaloReader
+from haloes.toolset.halo_reader import HaloReader
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        '--input_dir',
+        '-i',
+        '--input',
         type=str,
         default='color_trees',
         help='Path to the directory with merger tree files to process'
@@ -20,21 +21,12 @@ def main():
     Path('output/position').mkdir(parents=True, exist_ok=True)
     Path('output/v_circ').mkdir(parents=True, exist_ok=True)
     color_trees = [
-        os.path.join(args.input_dir, filename)
-        for filename in os.listdir(args.input_dir)
+        os.path.join(args.input, filename)
+        for filename in os.listdir(args.input)
     ]
-    processed = [
-        os.path.join('processed', filename)
-        for filename in os.listdir('processed')
-    ]
-    to_process = [filepath for filepath in color_trees
-                  if os.path.join(
-                      'processed',
-                      os.path.basename(filepath).replace('.hdf5', '.csv')
-                  ) not in processed]
     color_trees.sort()
     with Pool(processes=min(len(color_trees), cpu_count())) as pool:
-        processed += pool.map(HaloReader.process_tree, to_process)
+        processed = pool.map(HaloReader.process_tree, color_trees)
         print('Plotting graphs...')
         _ = pool.map(HaloReader.plot_tree, processed)
 
